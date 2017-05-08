985238895bffce7d5d932dd879bc99236a19b4c3
pomko.ky@gmail.com <1684PaukfreeX>
2017-05-08 16:43:20 +0400

    Create MainActivity class and main_layout

---
+++ app/src/main/java/com/kubyshkin/android/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.kubyshkin.android.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Wladimir on 08.05.2017.
+ */
+
+public class MainActivity extends Activity{
+
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
