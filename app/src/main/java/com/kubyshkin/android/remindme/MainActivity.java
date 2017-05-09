2cce04b82b4c4b33a1f96f27c2088057ea0df496
pomko.ky@gmail.com <1684PaukfreeX>
2017-05-09 14:15:15 +0400

    Start to implementing NavigationView

--- app/src/main/java/com/kubyshkin/android/remindme/MainActivity.java
+++ app/src/main/java/com/kubyshkin/android/remindme/MainActivity.java
@@ -1,9 +1,9 @@
 package com.kubyshkin.android.remindme;
 
-import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.Toolbar;
 
@@ -11,10 +11,11 @@ import android.widget.Toolbar;
  * Created by Wladimir on 08.05.2017.
  */
 
-public class MainActivity extends Activity{
+public class MainActivity extends AppCompatActivity{
     private Toolbar toolbar;
 
     protected void onCreate(Bundle saveInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_main);
 
